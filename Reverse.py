ea6ec4efab992d9ceffd9de616d305a8b5ba5982
Tanisha Rathore <tanisha.rathore572003@gmail.com>
2024-07-21 15:46:25 +0530

    Added Task 11

--- Reverse.py
+++
@@ -1,8 +0,0 @@
-def reverse_string():
-    user_string = input("Enter a string: ")
-    
-    reversed_string = user_string[::-1]
-    
-    print("The reversed string is:", reversed_string)
-
-reverse_string()
--- Square.py
+++
@@ -1,7 +0,0 @@
-def list_of_squares():
-    squares = [x ** 2 for x in range(1, 11)]
-    return squares
-
-if __name__ == "__main__":
-    squares = list_of_squares()
-    print(squares)
